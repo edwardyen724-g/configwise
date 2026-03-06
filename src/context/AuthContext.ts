402200bde50015197cc9518992d1ba481ce4943a
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 04:33:11 -0800

    feat: add missing module src/context/AuthContext.ts

---
+++ src/context/AuthContext.ts
@@ -0,0 +1,9 @@
+import React, { createContext, useContext } from 'react';
+
+const AuthContext = createContext(null);
+
+export const AuthProvider: React.FC = ({ children }) => {
+    return <AuthContext.Provider value={null}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
